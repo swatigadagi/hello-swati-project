2fe117ae380120b4ccaf0a261054258cebe840bb
Sumati A Gadagi <134225744+swatigadagi@users.noreply.github.com>
2025-12-29 17:24:54 +0530

    Delete frontend/main.js

--- frontend/main.js
+++
@@ -1,9 +0,0 @@
-// frontend/main.js
-fetch("/api/greet")
-  .then((response) => response.json())
-  .then((data) => {
-    document.getElementById("greeting").innerText = data.message;
-  })
-  .catch(() => {
-    document.getElementById("greeting").innerText = "Hello Swati (offline)";
-  });
